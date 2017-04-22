5a12c0bfcaae9ba4cbf18949f46504a4894e8d33
David Burles <dburles@gmail.com>
2017-04-22 16:15:23 +1000

    basic webpack project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    open: true,
+    port: 3000,
+  },
+};
