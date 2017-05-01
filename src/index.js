996e55adae4b0139bdfa999554fb5cf4ff0a30df
David Burles <dburles@gmail.com>
2017-05-01 19:44:17 +1000

    simple af

--- src/index.js
+++ src/index.js
@@ -1,51 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { nest } from "recompose";
-import router, { history, subscribe } from "./router";
-import Router, { render } from "./router-react";
-import Layout from "./containers/Layout";
-import Home from "./components/Home";
-import PageOne from "./containers/PageOne";
-import PageTwo from "./components/PageTwo";
-import NotFound from "./components/NotFound";
-import QueryStringTest from "./containers/QueryStringTest";
-import arrayRegex from "./router-array-regex";
-
-const routes = [
-  /^\/$/,
-  () => render(nest(Layout, Home)),
-
-  /^\/page-one$/,
-  () => render(nest(Layout, PageOne)),
-
-  /^\/page-one\/(\w+)$/,
-  ([, one]) => render(nest(Layout, PageOne), { one }),
-
-  /^\/page-one\/(\w+)\/(\w+)$/,
-  ([, one, two]) => render(nest(Layout, PageOne), { one, two }),
-
-  /^\/page-two$/,
-  () => render(nest(Layout, PageTwo)),
-
-  // /^\/page-one\/(\w+)\/(\w+)$/,
-  // ([, one, two]) => console.log("one with params:", { one, two }),
-
-  /^\/query-string-test.*$/,
-  () => render(nest(Layout, QueryStringTest)),
-
-  /^\/redirect-to-page-one\/(\w+)$/,
-  ([, test]) => history.push(`/page-one/${test}`),
-
-  // /^\/redirect-to-page-one\/(\w+)$/,
-  // ([, test]) => console.log(test),
-
-  /.*/,
-  () => render(nest(Layout, NotFound)),
-];
+import router, { subscribe } from "./router";
 
 // Log route changes
 subscribe(({ path, params }) => console.debug("route: ", { path, params }));
 
-router(arrayRegex(routes));
+router(path => console.log(path));
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+// ReactDOM.render(<Router />, document.getElementById("root"));
